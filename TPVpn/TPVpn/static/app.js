1855e7b935aa15fd5326169ae8c3dda08f9ba5dd
adamora <adam.mora.tortajada@gmail.com>
2017-02-15 20:05:40 +0100

    Actualización de licencias

--- TPVpn/TPVpn/static/app.js
+++ TPVpn/TPVpn/static/app.js
@@ -1,3 +1,21 @@
+//    --- License block --------------------------------------------------------
+//    This file is part of TPVpn.
+//
+//    TPVpn is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    TPVpn is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with TPVpn.  If not, see <http://www.gnu.org/licenses/>.
+//    --- End of License block -------------------------------------------------
+
+
 (function() {
   'use strict';
   angular.module('TPVpnapp',[
--- TPVpn/TPVpnapp/tables.py
+++ TPVpn/TPVpnapp/tables.py
@@ -1,5 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#    -*- coding: UTF-8 -*-
+#
+#    --- License block --------------------------------------------------------
+#    This file is part of TPVpn.
+#
+#    TPVpn is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    TPVpn is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with TPVpn.  If not, see <http://www.gnu.org/licenses/>.
+#    --- End of License block -------------------------------------------------
+
 
 import django_tables2 as tables
 
